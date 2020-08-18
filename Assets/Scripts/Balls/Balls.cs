a0b624cbdc39e77ba539154028dbbef33bab996f
Lars Koreska <larskoreska@gmail.com>
2020-08-18 11:14:01 +0200

    Fireball RAIN
    Fireballllssss

---
+++ Assets/Scripts/Balls/Balls.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balls : MonoBehaviour
+{
+
+    public float explosionTimer;
+
+
+
+    private void OnCollisionEnter(Collision col)
+    {
+        Destroy(gameObject, explosionTimer);
+    }
+}
