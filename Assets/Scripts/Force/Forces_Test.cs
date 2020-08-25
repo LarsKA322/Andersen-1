66c59825575674e4e40951da4ad11b774827b563
Lars Koreska <larskoreska@gmail.com>
2020-08-25 13:52:21 +0200

    lidt til godte posen
    jajaja

---
+++ Assets/Scripts/Force/Forces_Test.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Forces_Test : MonoBehaviour
+{
+    public Vector3 localVelo;
+    private Rigidbody rigidbody;
+
+    private void Start()
+    {
+        rigidbody = this.GetComponent<Rigidbody>();    
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //localVelo = transform.InverseTransformDirection(rigidbody.velocity);
+        Debug.Log(rigidbody.velocity);
+    }
+}
